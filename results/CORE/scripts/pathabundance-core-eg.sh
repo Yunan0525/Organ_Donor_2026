36949248551753ce7fef578d6804d56dbb9aeb2f
Yunan Hu <108050015+Yunan0525@users.noreply.github.com>
2026-05-26 10:02:55 -0400

    Delete results/CORE/scripts/pathabundance-core-eg.sh

--- results/CORE/scripts/pathabundance-core-eg.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-awk -F"\t" '($2>0.0001)' CORE-DIR/Lumen-AscendingColon/*_pathabundance-relab_unstratified.tsv | cut -f 1 | grep -v '^#' | tr " " "-" | sort | uniq -c | sed 's/^ \+//g' | tr " " "\t" | awk -F"\t" '($1>=4) {print $2;}' > pathabundance-Lumen-AscendingColon.txt
