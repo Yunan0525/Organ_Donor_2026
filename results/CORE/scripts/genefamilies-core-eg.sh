e21aac7b4a21754d6d3348bf7d06aada3875ff10
Yunan Hu <108050015+Yunan0525@users.noreply.github.com>
2026-05-26 10:02:43 -0400

    Delete results/CORE/scripts/genefamilies-core-eg.sh

--- results/CORE/scripts/genefamilies-core-eg.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-awk -F"\t" '($2>0.0001)' CORE-DIR/Mucosa-AscendingColon/*_genefamilies-relab_unstratified.tsv | cut -f 1 | grep -v '^#' | tr " " "-" | sort | uniq -c | sed 's/^ \+//g' | tr " " "\t" | awk -F"\t" '($1>=4) {print $2;}' > genefamilies-Mucosa-AscendingColon.txt
